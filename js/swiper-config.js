9e6f55160e9719c7b340dc01e96cb5f850beda4f
Maxim Toropchin <vimevi57@gmail.com>
2023-09-09 00:14:17 +0300

    fixed slider

--- js/swiper-config.js
+++ js/swiper-config.js
@@ -1,42 +1,89 @@
-// var init = false;
-// var swiper;
+// Исправленная версия -- прошлая версия
+
+// const resizebleSlider = () => {
+// 	s;
+// };
+
+// let init = false;
+// let swiper;
+
+// function initSwiper() {
+// 	swiper = new Swiper('.swiper', {
+// 		slidesPerView: 'auto',
+// 		pagination: {
+// 			el: '.swiper-pagination',
+// 			clickable: true,
+// 		},
+// 		// autoplay: {
+// 		// 	delay: 6000,
+// 		// },
+// 		keyboard: {
+// 			enabled: true,
+// 			onlyInViewport: true,
+// 		},
+// 		loop: false,
+// 		slideToClickedSlide: true,
+// 		spaceBetween: 16,
+// 		freeMode: true,
+// 	});
+// }
+
+// function destroySwiper() {
+// 	if (swiper && swiper.destroy) {
+// 		swiper.destroy(true, true);
+// 	}
+// }
+
 // function swiperCard() {
 // 	if (window.innerWidth <= 768) {
 // 		if (!init) {
+// 			initSwiper();
 // 			init = true;
-// 			swiper = new Swiper('.swiper', {
-// 				slidesPerView: 'auto',
-// 				pagination: {
-// 					el: '.swiper-pagination',
-// 					clickable: true,
-// 				},
-// 				// autoplay: { delay: 6000 },
-// 				keyboard: {
-// 					enabled: true,
-// 					onlyInViewport: true,
-// 				},
-// 				speed: 400,
-// 				slideToClickedSlide: true,
-// 				spaceBetween: 16,
-// 				freeMode: true,
-// 			});
 // 		}
 // 	} else if (init) {
-// 		swiper.slideTo(0); // Устанавливаем активный слайд с индексом 0 при ресайзе
-// 	} else {
+// 		destroySwiper();
 // 		init = false;
 // 	}
 // }
+
 // swiperCard();
 // window.addEventListener('resize', swiperCard);
 
-// Исправленная версия
+//////
 
-let init = false;
-let swiper;
+window.addEventListener('DOMContentLoaded', () => {
+	const resizableSwiper = (
+		breakpoint,
+		swiperClass,
+		swiperSettings,
+		callback
+	) => {
+		let swiper;
 
-function initSwiper() {
-	swiper = new Swiper('.swiper', {
+		breakpoint = window.matchMedia(breakpoint);
+
+		const enableSwiper = function (className, settings) {
+			swiper = new Swiper(className, settings);
+
+			if (callback) {
+				callback(swiper);
+			}
+		};
+
+		const checker = function () {
+			if (breakpoint.matches) {
+				return enableSwiper(swiperClass, swiperSettings);
+			} else {
+				if (swiper !== undefined) swiper.destroy(true, true);
+				return;
+			}
+		};
+
+		breakpoint.addEventListener('change', checker);
+		checker();
+	};
+
+	resizableSwiper('(max-width: 768px)', '.swiper', {
 		slidesPerView: 'auto',
 		pagination: {
 			el: '.swiper-pagination',
@@ -45,34 +92,12 @@ function initSwiper() {
 		// autoplay: {
 		// 	delay: 6000,
 		// },
-		keyboard: {
-			enabled: true,
-			onlyInViewport: true,
-		},
-		loop: false,
-		speed: 400,
-		slideToClickedSlide: true,
+		// keyboard: {
+		// 	enabled: true,
+		// 	onlyInViewport: true,
+		// },
+		// slideToClickedSlide: true,
 		spaceBetween: 16,
 		freeMode: true,
 	});
-}
-
-function destroySwiper() {
-	if (swiper && swiper.destroy) {
-		swiper.destroy(true, true);
-	}
-}
-
-function swiperCard() {
-	if (window.innerWidth <= 768) {
-		if (!init) {
-			initSwiper();
-			init = true;
-		}
-	} else if (init) {
-		destroySwiper();
-		init = false;
-	}
-}
-swiperCard();
-window.addEventListener('resize', swiperCard);
+});
